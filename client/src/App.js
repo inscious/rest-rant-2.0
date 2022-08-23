c5e54c69db13935e76c1dc40b054adc7d0a19743
Saul Cortes <92839820+inscious@users.noreply.github.com>
2022-08-23 13:27:10 -0700

    added hero section & fixed css

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import PlacesIndex from "./pages/Places";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 const App = () => {
     return (
         <Router className="">
-            <div className="container mx-auto max-w-7xl">
-                <Navbar />
-                <Routes>
-                    <Route exact path="/places" element={<PlacesIndex />} />
-                </Routes>
-            </div>
+            {/* <main className="container mx-auto max-w-7xl main-body"> */}
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/places" element={<PlacesIndex />} />
+                <Route exact path="/about" element={<About />} />
+            </Routes>
+            {/* </main> */}
         </Router>
     );
 };
